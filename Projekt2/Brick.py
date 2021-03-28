7a0e61d60a48acf53d53ff1cc2f062e265ff5736
piotr <piotrekp94@gmail.com>
2021-03-28 17:26:58 +0200

    Code Refactoring

---
+++ Projekt2/Brick.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class Brick(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.surf = pygame.Surface((20, 10))
+        self.surf = pygame.image.load("images/klocek.png").convert()
+        self.rect = self.surf.get_rect(center=(x, y))
+
+    def set_kolor(self, color):
+        self.surf.fill(color)
---
+++ Projekt2/Player.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import KEYDOWN, K_ESCAPE, K_UP, K_DOWN, K_LEFT, K_RIGHT, QUIT
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, width):
+        pygame.sprite.Sprite.__init__(self)
+        self.surf = pygame.Surface((30, 10))
+        self.surf.fill((255, 0, 0))
+        self.surf = pygame.image.load("images/belka.png").convert()
+        self.rect = self.surf.get_rect(center=(x, y))
+        self.screenWidth = width
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-3, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(3, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > self.screenWidth - 30:
+            self.rect.right = self.screenWidth - 30
