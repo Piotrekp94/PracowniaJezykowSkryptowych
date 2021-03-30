8960881abe53cf5f779f87a8da10757e47ed50b6
piotr <piotrekp94@gmail.com>
2021-03-30 20:16:47 +0200

    Add last level and pausing

--- Projekt2/GameLoop.py
+++ Projekt2/GameLoop.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import KEYDOWN, K_ESCAPE, K_n
+from pygame.locals import KEYDOWN, K_ESCAPE, K_n, K_p
 
 from Projekt2.Ball import Ball
 from Projekt2.LevelManager import LevelManager
@@ -9,6 +9,7 @@ from Projekt2.Player import Player
 class GameLoop:
 
     def __init__(self, width, height):
+        self.gameIsPaused = False
         self.width = width
         self.height = height
         self.isPlaying = True
@@ -18,7 +19,7 @@ class GameLoop:
 
         self.levelManager = LevelManager(width, height)
         self.clock = pygame.time.Clock()
-        self.currentLevel = 4
+        self.currentLevel = 1
         self.bricks = self.levelManager.getMapLevel(self.currentLevel)
 
     def isGameOn(self):
@@ -26,12 +27,16 @@ class GameLoop:
 
     def next(self):
         self.handleInput()
-
-        pressed_keys = pygame.key.get_pressed()
-        self.player.update(pressed_keys)
-        self.draw()
-        self.handleCollision()
-        self.ball.move()
+        if self.gameIsPaused is False:
+            pressed_keys = pygame.key.get_pressed()
+            self.player.update(pressed_keys)
+            self.draw()
+            if self.ball is not None:
+                self.handleCollision()
+                self.ball.move()
+        else:
+            self.draw()
+            self.drawPausedText()
         self.clock.tick(60)
         pygame.display.flip()
 
@@ -42,15 +47,28 @@ class GameLoop:
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     self.isPlaying = False
-                if event.key == K_n:
+                if event.key == K_n and not self.gameIsPaused:
                     self.loadNextLevel()
+                if event.key == K_p:
+                    self.gameIsPaused = not self.gameIsPaused
 
     def draw(self):
         self.screen.fill((0, 0, 0))
         for brick in self.bricks:
             self.screen.blit(brick.surf, brick.rect)
-        self.screen.blit(self.player.surf, self.player.rect)
-        self.screen.blit(self.ball.surf, self.ball.rect)
+        if self.player is not None:
+            self.screen.blit(self.player.surf, self.player.rect)
+        if self.ball is not None:
+            self.screen.blit(self.ball.surf, self.ball.rect)
+
+        if self.currentLevel > 5:
+            font = pygame.font.Font(pygame.font.get_default_font(), 30)
+            text_surface = font.render('Thank you for playing', True, (155, 2, 155))
+            text_rect = text_surface.get_rect(center=(self.width / 2, self.height / 3))
+            self.screen.blit(text_surface, text_rect)
+            text_surface = font.render('Your Score is: ', True, (155, 2, 155))
+            text_rect = text_surface.get_rect(center=(self.width / 2, self.height / 3 * 2))
+            self.screen.blit(text_surface, text_rect)
 
     def handleCollision(self):
         # collidingObject = pygame.sprite.spritecollideany(self.ball, self.bricks)
@@ -61,10 +79,22 @@ class GameLoop:
         pass
 
     def loadNextLevel(self):
-        if self.currentLevel < 6:
-            self.currentLevel += 1
-            self.bricks = self.levelManager.getMapLevel(self.currentLevel)
+        if self.currentLevel < 5:
+            self.bricks = self.levelManager.getMapLevel(self.currentLevel + 1)
             self.ball = Ball(315, 440)
-            print(self.currentLevel)
-        if self.currentLevel >= 6:
-            self.bricks = []
+        if self.currentLevel == 5:
+            self.loadLastLevel()
+        if self.currentLevel > 5:
+            self.isPlaying = False
+        self.currentLevel += 1
+
+    def loadLastLevel(self):
+        self.bricks = []
+        self.ball = None
+
+    def drawPausedText(self):
+        font = pygame.font.Font(pygame.font.get_default_font(), 36)
+        # now print the text
+        text_surface = font.render('Paused', True, (155, 2, 155))
+        text_rect = text_surface.get_rect(center=(self.width / 2, self.height / 2))
+        self.screen.blit(text_surface, text_rect)
